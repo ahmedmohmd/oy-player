cb8da3f4790e6528f0c1006ca38c235c23d8b884
Ahmed Mohamed <er.ahmed.3301@gmail.com>
2022-06-27 05:44:57 +0200

    add time icon to audio controle

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -87,7 +87,21 @@ function AudioPlayer({ tracks }) {
           onNext={handleToNextTrack}
           onPlayPause={setIsPlaying}
         />
-        <div className="w-full">
+        <div className="inline-flex items-center justify-center w-full gap-3">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-7 h-7"
+              viewBox="0 0 20 20"
+              fill="#fff"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
           <input
             type="range"
             value={trackProgress}
